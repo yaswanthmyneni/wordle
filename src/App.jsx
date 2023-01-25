d0d6fcdcd7301cd16b0f4f8cbc21d8309ac019b8
yaswanthmyneni <yaswanthmyneni@gmail.com>
2023-01-25 21:57:25 +0530

    fix: responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const { theme, setTheme } = useThemeContext();
   return (
     <main className={`h-[100vh] ${theme === "dark" ? "bg-gray-700" : ""}`}>
-      <div className="relative">
+      <div className="sm:relative">
         <h1
           className={`text-3xl text-center mb-4 pt-4 ${
             theme === "dark" ? "text-white" : ""
@@ -16,7 +16,7 @@ const App = () => {
           Wordle App
         </h1>
         <button
-          className="px-2 py-1 rounded bg-slate-400 text-white absolute top-4 right-2"
+          className="px-2 py-1 rounded bg-slate-400 text-white block ml-auto mb-8  sm:absolute sm:top-4 sm:right-2"
           onClick={() => {
             if (theme === "dark") {
               setTheme("light");
